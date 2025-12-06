c9bbce287227e4ba640075f27f2afa782835cf56
Beau Badilla <beau.badilla@gmail.com>
2025-12-05 16:56:26 -0800

    add initial db script

---
+++ scripts/init_database.sql
@@ -0,0 +1,25 @@
+USE master;
+GO
+
+-- Drop database if it exists
+IF EXISTS (SELECT 1 FROM sys.databases WHERE name = 'DataWarehouse')
+BEGIN
+	ALTER DATABASE DataWarehouse SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+	DROP DATABASE DataWarehouse;
+END;
+GO
+
+-- Create database
+CREATE DATABASE DataWarehouse;
+GO
+
+USE DataWarehouse;
+GO
+
+-- Create schemas
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
