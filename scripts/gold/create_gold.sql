2b086b5f607df3ffc64c26526a573e9b860af2b7
Beau Badilla <beau.badilla@gmail.com>
2025-12-19 14:38:45 -0800

    syntax

--- scripts/gold/create_gold.sql
+++ scripts/gold/create_gold.sql
@@ -23,9 +23,9 @@ GO
 
 CREATE VIEW gold.dim_customers AS
 SELECT
-    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key, -- Surrogate key
-    ci.cst_id AS customer_id,
-    ,ci.cst_key AS customer_number,
+    ROW_NUMBER() OVER (ORDER BY cst_id) AS customer_key -- Surrogate key
+    ,ci.cst_id AS customer_id
+    ,ci.cst_key AS customer_number
     ,ci.cst_firstname AS first_name
     ,ci.cst_lastname AS last_name
     ,la.cntry AS country
@@ -35,8 +35,8 @@ SELECT
         ELSE COALESCE(ca.gen, 'n/a')  			   -- Fallback to ERP data
     END AS gender
     ,ca.bdate AS birthdate
-    ,ci.cst_create_date AS create_dat
-,FROM silver.crm_cust_info ci
+    ,ci.cst_create_date AS create_date
+FROM silver.crm_cust_info ci
 LEFT JOIN silver.erp_cust_az12 ca
     ON ci.cst_key = ca.cid
 LEFT JOIN silver.erp_loc_a101 la
@@ -52,7 +52,7 @@ GO
 
 CREATE VIEW gold.dim_products AS
 SELECT
-    ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key, -- Surrogate ke
+    ROW_NUMBER() OVER (ORDER BY pn.prd_start_dt, pn.prd_key) AS product_key -- Surrogate key
     ,pn.prd_id AS product_id
     ,pn.prd_key AS product_number
     ,pn.prd_nm AS product_name
@@ -62,8 +62,8 @@ SELECT
     ,pc.maintenance AS maintenance
     ,pn.prd_cost AS cost
     ,pn.prd_line AS product_line
-    ,pn.prd_start_dt AS start_dat
-,FROM silver.crm_prd_info pn
+    ,pn.prd_start_dt AS start_date
+FROM silver.crm_prd_info pn
 LEFT JOIN silver.erp_px_cat_g1v2 pc
     ON pn.cat_id = pc.id
 WHERE pn.prd_end_dt IS NULL; -- Filter out all historical data
@@ -86,8 +86,8 @@ SELECT
     ,sd.sls_due_dt AS due_date
     ,sd.sls_sales AS sales_amount
     ,sd.sls_quantity AS quantity
-    ,sd.sls_price AS pric
-,FROM silver.crm_sales_details sd
+    ,sd.sls_price AS price
+FROM silver.crm_sales_details sd
 LEFT JOIN gold.dim_products pr
     ON sd.sls_prd_key = pr.product_number
 LEFT JOIN gold.dim_customers cu
